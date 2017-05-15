d83039865d7b22a1619ab0bd461c46999571e05c
Johnny <txtstudioscompany@gmail.com>
2017-05-15 18:19:14 -0400

    Add files via upload

---
+++ Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿/*-------------------------------------------------------------------------------------------------
+       dP          dP                                     dP                          dP       
+       88          88                                     88                          88       
+       88 .d8888b. 88d888b. 88d888b. 88d888b. dP    dP    88        .d8888b. .d8888b. 88  .dP  
+       88 88'  `88 88'  `88 88'  `88 88'  `88 88    88    88        88ooood8 88ooood8 88888"   
+88.  .d8P 88.  .88 88    88 88    88 88    88 88.  .88    88        88.  ... 88.  ... 88  `8b. 
+ `Y8888'  `88888P' dP    dP dP    dP dP    dP `8888P88    88888888P `88888P' `88888P' dP   `YP 
+                                                   .88                                         
+                                               d8888P                                         
+-------------------------------------------------------------------------------------------------
+
+This is arguably the most important class in the game. It is the main hub for every aspect of the
+game. It gives the item blocks their id's, which tells them what item to give, as well as handling
+the main mechanics of the game, counting score, keeping track of enemy movements, and keeping the
+game running in general. It handles most of the startup code, and initializes most of the game.
+
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject itemBlockContainer;
+    public Text coinText;
+
+    private void Start()
+    {
+        Debug.Log("Initializing Game.");
+        DontDestroyOnLoad(transform.gameObject); //Keep this instance of the object running so that variables don't change.
+
+        Debug.Log("Found " + itemBlockContainer.transform.childCount + " children for " + itemBlockContainer.name);
+        Debug.Log("    Assigning ids to children");
+        for(int i = 0; i < itemBlockContainer.transform.childCount; i++)
+        {
+            Debug.Log("    Child " + i + " of " + itemBlockContainer.name + " found.");
+            ItemBlock newblock = itemBlockContainer.transform.GetChild(i).gameObject.GetComponent<ItemBlock>();
+            Debug.Log("        Giving child box id: " + i);
+            newblock.boxid = i;
+            newblock.notifyForId();
+        }
+
+        coinText = coinText.GetComponent<Text>();
+        coinText.text = "Coins: " + 0;
+
+    }
+    
+}
---
+++ Scripts/MainMenuListener.cs
@@ -0,0 +1,37 @@
+﻿/*-------------------------------------------------------------------------------------------------
+       dP          dP                                     dP                          dP       
+       88          88                                     88                          88       
+       88 .d8888b. 88d888b. 88d888b. 88d888b. dP    dP    88        .d8888b. .d8888b. 88  .dP  
+       88 88'  `88 88'  `88 88'  `88 88'  `88 88    88    88        88ooood8 88ooood8 88888"   
+88.  .d8P 88.  .88 88    88 88    88 88    88 88.  .88    88        88.  ... 88.  ... 88  `8b. 
+ `Y8888'  `88888P' dP    dP dP    dP dP    dP `8888P88    88888888P `88888P' `88888P' dP   `YP 
+                                                   .88                                         
+                                               d8888P                                         
+-------------------------------------------------------------------------------------------------
+
+This class is for the Main Menu. It handles the button clicks and changes scenes / ui accordingly
+
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuListener : MonoBehaviour {
+
+    private Button playButton;
+
+    private void Start()
+    {
+        
+        playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        playButton.onClick.AddListener(PlayButtonClick);
+    }
+
+    void PlayButtonClick()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
